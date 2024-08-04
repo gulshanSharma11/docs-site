bd9befa4e5a0f4ede7f24255e28a7dd61c73d79a
GULSHAN SHARMA <72249304+gulshanSharma11@users.noreply.github.com>
2024-08-04 15:01:28 +0530

    "Update README.md with new content, including website description, installation, local development, build, and deployment instructions."

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("/Users/gulshansharma/vscode/Github Desktop/docs-site/node_modules/infima/dist/css/default/default.css"),
+  require("/Users/gulshansharma/vscode/Github Desktop/docs-site/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/Users/gulshansharma/vscode/Github Desktop/docs-site/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/Users/gulshansharma/vscode/Github Desktop/docs-site/src/css/custom.css"),
+];
